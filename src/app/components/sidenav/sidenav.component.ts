1aff40a422d83ca96344ae2b61f451b07059231c
Nicolas <nicolasmeireles1004@gmail.com>
2024-06-07 19:25:21 -0300

    style: :poop:

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-
+  nameGames: Array<string> = ['Roleta da Picanha 🥩', 'Game 2', 'Game 3', 'Game 4', 'Game 5'];
 }

9a641083e5edfda58dae4e85ee37e477f98a5398
Nicolas <nicolasmeireles1004@gmail.com>
2024-06-07 18:55:17 -0300

    style: :poop:

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { SidenavComponent } from '../../components/sidenav/sidenav.component';
+import { BannerComponent } from '../../components/banner/banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent, SidenavComponent, BannerComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'

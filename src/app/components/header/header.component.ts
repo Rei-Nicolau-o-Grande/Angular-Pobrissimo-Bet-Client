d8edf248160fcfc8d8fdf13fcac29fce7ea64d5b
Nicolas <nicolasmeireles1004@gmail.com>
2024-06-07 23:11:48 -0300

    style: :poop:

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,13 +2,15 @@ import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { FormAuthComponent } from '../form-auth/form-auth.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     MatIconModule,
-    MatDialogModule
+    MatDialogModule,
+    CommonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -16,13 +18,18 @@ import { FormAuthComponent } from '../form-auth/form-auth.component';
 export class HeaderComponent {
   private dialogService = inject(MatDialog);
   @Output() eventFormAuth: any = new EventEmitter();
+  public isMenuOpen: boolean = false;
 
   public handleOpenModal(isLoginOrCreateUser: boolean): void {
     this.dialogService.open(FormAuthComponent, {
       width: '700px',
       height: '400px',
       data: isLoginOrCreateUser
-    })
+    });
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
   public sendEventFormAuth(event: boolean): void {
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule, MatIconModule
   ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
+
   nameGames: Array<string> = ['Roleta da Picanha 🥩', 'Game 2', 'Game 3', 'Game 4', 'Game 5'];
+
+  @Input() isOpen: boolean = false;
+  @Output() closeSidenav = new EventEmitter<void>();
+
+  toggleSidenav() {
+    this.isOpen = !this.isOpen;
+  }
+
+  close() {
+    this.closeSidenav.emit();
+  }
 }
